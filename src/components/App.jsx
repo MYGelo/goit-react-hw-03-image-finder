ad735886456da047b00eaed3fc21163a6117fe0b
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-03-21 03:22:49 +0200

    day6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,85 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { ImageGalleryItem } from 'components/ImageGalleryItem';
+import { fetchImages } from 'components/Api/Api';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { Btn } from './Button/Button';
 
 export class App extends Component {
   state = {
     inputSearch: '',
+    error: null,
+    isLoading: false,
+    images: [],
+    pageNr: 1,
+    showModal: false,
+    imgSrc: '',
+    imgAlt: '',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevState.inputSearch;
+    const currentSearch = this.state.inputSearch;
+
+    if (prevSearch !== currentSearch) {
+      this.setState({
+        inputSearch: currentSearch,
+      });
+      fetchImages(currentSearch, 1)
+        .then(images => this.setState({ images, isLoading: false }))
+        .catch(error => this.setState({ error }));
+    }
+  }
+  onClickMore = async () => {
+    const { inputSearch, pageNr, images } = this.state;
+    const response = await fetchImages(inputSearch, pageNr + 1);
+    this.setState({
+      images: [...images, ...response],
+      PageNr: pageNr + 1,
+    });
   };
 
   handleSearchSubmit = inputSearch => {
     this.setState({ inputSearch });
+    this.setState(({ isLoading }) => ({
+      isLoading: !isLoading,
+    }));
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  onOpenModal = e => {
+    this.setState({
+      showModal: true,
+      imgSrc: e.target.name,
+      imgAlt: e.target.alt,
+    });
+  };
+
+  onCloseModal = e => {
+    e.stopPropagation();
+    this.setState({
+      showModal: false,
+      imgSrc: '',
+      imgAlt: '',
+    });
+  };
+  onClick = e => {
+    console.log('onClick');
   };
 
   render() {
+    const { images, isLoading, showModal, imgSrc, imgAlt } = this.state;
+    console.log('showModal:', showModal);
+
     return (
       <>
-        <Searchbar onSubmit={this.handleSearchSubmit} />
         <div
           style={{
             height: '100vh',
@@ -22,7 +87,27 @@ export class App extends Component {
             color: '#010101',
           }}
         >
-          <ImageGallery inputSearch={this.state.inputSearch} />
+          <Searchbar onSubmit={this.handleSearchSubmit} />
+
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <React.Fragment>
+              <ImageGallery images={images} onClick={this.onOpenModal} />
+              <button onClick={this.onOpenModal}>1111111</button>
+
+              {images.length > 11 ? <Btn onClick={this.onClickMore} /> : null}
+            </React.Fragment>
+          )}
+
+          {showModal ? (
+            <Modal>
+              <button type="button" onClick={this.onCloseModal}>
+                123
+              </button>
+              <img onClick={this.onCloseModal} src={imgSrc} alt={imgAlt} />
+            </Modal>
+          ) : null}
         </div>
       </>
     );
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,12 +1,20 @@
 import css from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
 
 export const ImageGalleryItem = ({ image, onClick }) => (
-  <li className={css.ImageGalleryItem} id={image.id} onClick={onClick}>
-    <img
-      src={image.webformatURL}
-      alt={image.tags}
-      name={image.largeImageURL}
-      className={css.ImageGalleryItem__image}
-    ></img>
-  </li>
+  <button type="button" onClick={e => onClick}>
+    <li className={css.ImageGalleryItem} id={image.id}>
+      <img
+        src={image.webformatURL}
+        alt={image.tags}
+        name={image.largeImageURL}
+        className={css.ImageGalleryItem__image}
+      ></img>
+    </li>
+  </button>
 );
+
+ImageGalleryItem.propTypes = {
+  image: PropTypes.object.isRequired,
+  onclick: PropTypes.func.isRequired,
+};
